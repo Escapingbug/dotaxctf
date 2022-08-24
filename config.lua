71e39c56f5f622f874dbad7f8f2bae1f3cd64b7c
Anciety <anciety@pku.edu.cn>
2022-08-24 23:38:30 +0800

    implement team splits. implement bot player so that scores can be collected by last_hit now

--- config.lua
+++ config.lua
@@ -6,10 +6,15 @@ function Config:Init()
     self.config_inited = true
 
     self.extra_score_for_winner = 1
+    self.team_player_count = 1
     self.teams = {
         [19] = "team1",
         [20] = "team2",
     }
+    self.team_count = 0
+    for _, _ in pairs(self.teams) do
+        self.team_count = self.team_count + 1
+    end
     self.round_time = 60
     self.hero_locations = {
         [19] = Vector(100, 100),
--- rounds.lua
+++ rounds.lua
@@ -1,6 +1,21 @@
 require("config")
 require("bot_script_env")
 require("lib/timer")
+require("lib/inspect")
+
+AVAILABLE_TEAMS = {
+    DOTA_TEAM_GOODGUYS,
+    DOTA_TEAM_BADGUYS,
+    DOTA_TEAM_NEUTRALS,
+    DOTA_TEAM_CUSTOM_1,
+    DOTA_TEAM_CUSTOM_2,
+    DOTA_TEAM_CUSTOM_3,
+    DOTA_TEAM_CUSTOM_4,
+    DOTA_TEAM_CUSTOM_5,
+    DOTA_TEAM_CUSTOM_6,
+    DOTA_TEAM_CUSTOM_7,
+    DOTA_TEAM_CUSTOM_8,
+}
 
 if Rounds == nil then
     Rounds = class({})
@@ -15,6 +30,12 @@ function Rounds:Init()
     self.scores_this_round = {}
     -- team number => hero object
     self.heros = {}
+
+    self.available_players = {}
+
+    -- player id to team number
+    self.player_to_team = {}
+    self.team_to_player = {}
 end
 
 function Rounds:InitGameMode()
@@ -47,6 +68,43 @@ function Rounds:CleanupLivingHeros()
     self.heros = {}
 end
 
+--[[
+    As we need bot player to know who is the killer,
+    we setup the bot players with fake heros first.
+]]
+function Rounds:SetupBotPlayers()
+    print(".. debug ? " .. tostring(debug.sethook))
+    for team_id, team_name in pairs(Config.teams) do
+        local ob_hero = GameRules:AddBotPlayerWithEntityScript(
+            "npc_dota_hero_abaddon",
+            team_name,
+            DOTA_TEAM_GOODGUYS,
+            "bot/ob_hero_act.lua",
+            false
+        )
+
+        assert(ob_hero, "add bot player failed")
+
+        ob_hero:SetRespawnsDisabled(true)
+        ob_hero:ForceKill(false)
+        local player_id = ob_hero:GetPlayerID()
+        print("adding player id " .. tostring(player_id) .. " to team " .. tostring(team_id))
+        self.player_to_team[player_id] = team_id
+        self.team_to_player[team_id] = player_id
+    end
+end
+
+function Rounds:SetupLastHitListener()
+    ListenToGameEvent("last_hit", function(event)
+        local entity_killed = event["EntKilled"]
+        local player_id = event["PlayerID"]
+        local team_name = self.player_to_team[player_id]
+        -- TODO: add scores
+        print("got last hit! " .. entity_killed .. " " .. player_id)
+        print("last hit team name " .. tostring(team_name))
+    end, nil)
+end
+
 function Rounds:BeginGame()
     if not self.game_started then
         -- TODO: use add bot player with entity script to add
@@ -55,11 +113,8 @@ function Rounds:BeginGame()
         -- Explain: last_hit can only get you the player id instead
         -- of the entity id.
 
-        ListenToGameEvent("last_hit", function (event)
-            local entity_killed = event["EntKilled"]
-            local player_id = event["PlayerID"]
-            print("got last hit! " .. entity_killed .. " " .. player_id)
-        end, nil)
+        Rounds:SetupBotPlayers()
+        Rounds:SetupLastHitListener()
 
         Rounds:PrepareBeginRound()
         self.game_started = true
@@ -112,28 +167,67 @@ function Rounds:PrepareRoundPlayerScripts(on_done)
     end)
 end
 
+function table.shuffle(x)
+	for i = #x, 2, -1 do
+		local j = math.random(i)
+		x[i], x[j] = x[j], x[i]
+	end
+
+    return x
+end
+
+function table.clone(list)
+    return {table.unpack(list)}
+end
+
 function Rounds:ChooseHeros(chooser_scripts)
     -- TODO: http fetch the real hero chooser code
     -- TODO: add hero chooser fetch flag so that game only starts
     -- when hero is added
     print("choosing heros")
-    for team_num, team_name in pairs(Config.teams) do
-        local chooser = load(chooser_scripts[team_num])()
-        print("chooser " .. tostring(chooser))
-        if chooser ~= nil then
-            local hero_name = chooser(self.round_count)
-            local team_hero = CreateUnitByName(
-                hero_name,
-                Config.hero_locations[team_num],
-                true, -- findClearSpace
-                nil, -- npcowner
-                nil, -- entity owner
-                DOTA_TEAM_NOTEAM
-            )
-            print("new hero for " .. team_name .. ": " .. tostring(team_hero) .. " id: " .. team_hero:GetHeroID())
 
-            Rounds:InitTeamHero(team_hero)
-            self.heros[team_num] = team_hero
+    -- split teams
+    local team_count = Config.team_count / Config.team_player_count
+    local team_config = {}
+
+    for team_id, _ in pairs(Config.teams) do
+        table.insert(team_config, team_id)
+    end
+
+    team_config = table.shuffle(team_config)
+    print("team config " .. GameRules.inspect(team_config))
+
+    for i = 1, team_count do
+        -- cur_team: team_num (of ctf) for current team
+        local cur_team = {}
+        for _ = 1, Config.team_player_count do
+            table.insert(cur_team, table.remove(team_config, 1))
+        end
+
+        local cur_team_id = AVAILABLE_TEAMS[i]
+
+        for _, team_num in ipairs(cur_team) do
+            print("chooser of " .. tostring(team_num) .. tostring(chooser_scripts[team_num]))
+            local chooser = load(chooser_scripts[team_num])()
+            if chooser ~= nil then
+                local hero_name = chooser(self.round_count)
+                local player_id = self.team_to_player[team_num]
+                local player_owner = PlayerResource:GetPlayer(player_id)
+                print("player owner: " .. tostring(player_owner) .. "team id " .. tostring(cur_team_id))
+                local team_hero = CreateUnitByName(
+                    hero_name,
+                    Config.hero_locations[team_num],
+                    true, -- findClearSpace
+                    nil, -- npcowner
+                    player_owner, -- entity owner
+                    cur_team_id
+                )
+
+                print("check player and team .. " .. tostring(team_hero:GetPlayerID()) .. " " .. tostring(team_hero:GetTeam()))
+
+                Rounds:InitTeamHero(team_hero)
+                self.heros[team_num] = team_hero
+            end
         end
     end
 end
